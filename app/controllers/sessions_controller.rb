9ed9a7ea9e84ca002fbb54e2e966a46593387897
sueta1995 <kitaev.tagir@gmail.com>
2022-09-13 09:45:36 +0300

    edited sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user_params = params.require(:session)
+
+    user = User.find_by(email: user_params[:email])
+
+    if user.present?
+      session[:user_id] = user.id 
+      
+      redirect_to '/'
+    else
+      flash[:alert] = 'Неправильный email или пароль!'
+
+      render :new
+   end
   end
 end
