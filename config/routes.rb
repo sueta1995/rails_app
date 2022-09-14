a26dfd2997214f9208dc930703c7949b3b4e2086
sueta1995 <tagir.kitaev@inbox.ru>
2022-09-14 21:27:01 +0300

    contacts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'contacts/index'
   get 'yip/index'
   get 'main/index'
 
   get 'main', to: 'main#index'
   get 'yip', to: 'yip#index'
+  get 'contacts', to: 'contacts#index'
 
   resource :session, only: %i[new create]
   resources :users, only: %i[new create]
