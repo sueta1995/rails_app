e9aab1aa0f6f8cbe6e62c96ef99f0a253ca0d491
sueta1995 <kitaev.tagir@gmail.com>
2022-09-15 16:12:10 +0300

    upgraded authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,21 @@ class UsersController < ApplicationController
 
   def create
     user_params = params.require(:user).permit(:nickname, :email, :password)
+    user_check_nickname = User.find_by(nickname: user_params[:nickname])
+    user_check_email = User.find_by(email: user_params[:email])
 
-    User.create(user_params)
+    if user_check_nickname.present?
+      flash.now[:alert] = 'Пользователь с таким никнеймом уже существует!'
 
-    redirect_to root_path, notice: 'Вы успешно зарегистрировались'
+      render :new
+    elsif user_check_email.present?
+      flash.now[:alert] = 'Пользователь с такой почтой уже существует!'
+
+      render :new
+    else
+      User.create(user_params)
+
+      redirect_to root_path, notice: 'Вы успешно зарегистрировались'
+    end
   end
 end
