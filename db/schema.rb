a5eae8f869e71bc617d671861b76faa2e289c1a7
sueta1995 <kitaev.tagir@gmail.com>
2022-10-08 23:17:07 +0300

    added Subscription model and edited buttons in session/new and users/new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_150005) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_08_194352) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_150005) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_questions_on_user_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "user_id"
+    t.string "follower_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
