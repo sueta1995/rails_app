25d3d8ae280a084a7546c7c30abd7b78596ae5df
sueta1995 <tagir.kitaev@inbox.ru>
2022-12-16 21:37:34 +0300

    completed shadowban system

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -10,6 +10,8 @@ module QuestionsHelper
     return unless @question_required.present?
 
     @question_user = @question_required.user
-    @question_comments = @question_required.comments.select { |c| c.user.banned_user.nil? }.reverse
+    @question_comments = @question_required.comments.select { |c| c.user.banned_user.nil? && c.user.shadowbanned_user.nil? || c.user == current_user }.reverse
+    @question_user_banned = @question_user.banned_user.present?
+    @question_user_shadowbanned = @question_user.shadowbanned_user.present?
   end
 end
